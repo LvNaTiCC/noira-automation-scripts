a92a2a761bc0c3f5dda9cb843b54240e51f8d244
LvNaTiC <LvNaTiCC@proton.me>
2026-05-13 03:46:45 +0700

    Add edge_fw description

---
+++ edge_fw/terraform/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  default_user_account = {
+    username = var.default_ssh_user
+    keys     = [var.default_ssh_pubkey]
+  }
+
+  edge_vms = {
+    for idx in range(length(var.proxmox_nodes)) :
+    format("${var.edge_vm_prefix}-%02d", idx + 1) => {
+      ip = "edge-fw-.${idx + 1}"
+      node = var.proxmox_nodes[idx % length(var.proxmox_nodes)]
+    }
+  }
+
+  edge_hosts = [
+    for name, vm in local.edge_vms :
+    "${name} ansible_host=${vm.ip} ansible_user=${var.default_ssh_user}"
+  ]
+}
