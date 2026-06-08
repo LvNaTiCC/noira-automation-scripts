f8a9f08e37e288c7d4dda7500738ae97de5a5922
Luna <lvnaticc@proton.me>
2026-06-08 11:58:08 +0700

    finished work on chr vm initial setup

--- cloud-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ANSIBLE_HOST_KEY_CHECKING=False
-
-ansible-playbook bare-metal/pve/ansible/deploy-pve.yml -i pve_hosts.ini
-ansible-playbook bare-metal/net/ansible/deploy-network.yml -i pve_hosts.ini
-ansible-playbook templates/ansible/install-basic-templates -i pve_hosts.ini
