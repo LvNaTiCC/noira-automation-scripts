1e3d9222d6387f75d42b04fc27684d05d179bcf8
Luna <lvnaticc@proton.me>
2026-06-09 20:52:19 +0700

    a lot of work on chr internal configuration, works now

--- noira-deploy.sh
+++ noira-deploy.sh
@@ -5,3 +5,5 @@ ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook bare-metal/pve/ansible/deploy-pve.yml -i inventory.ini
 ansible-playbook bare-metal/net/ansible/deploy-network.yml -i inventory.ini
 ansible-playbook templates/ansible/install-basic-templates.yml -i inventory.ini
+
+#TODO Move all group-vars into a repo root, pass it via commands above
