a7d48883ec02d7d349ea28a066d2ef5eb363a644
Luna <lvnaticc@proton.me>
2026-06-09 21:57:50 +0700

    tweaks and fixes

--- noira-deploy.sh
+++ noira-deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-ANSIBLE_HOST_KEY_CHECKING=False
-
 ansible-playbook bare-metal/pve/ansible/deploy-pve.yml -i inventory.ini -e "@config_vars.yml"
 ansible-playbook bare-metal/net/ansible/deploy-network.yml -i inventory.ini -e "@config_vars.yml"
 ansible-playbook templates/ansible/install-basic-templates.yml -i inventory.ini -e "@config_vars.yml"
-
-#TODO Move all group-vars into a repo root, pass it via commands above
