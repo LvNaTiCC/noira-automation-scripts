2ac5d4df61fa971fa6e0ac51643ff254c26e8e27
Luna <lvnaticc@proton.me>
2026-06-14 07:40:30 +0700

    got rid of k8s, a lot of work on deployment and networking - set up proper firewall rules, ditched btrfs/zfs on root partition and ditched snapshots, for simplicity sake

--- noira-rollback-and-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ansible-playbook bare-metal/pve/ansible/rollback-to-clean-standalone.yml -i inventory.ini -e "@config_vars.yml"
-ansible-playbook bare-metal/pve/ansible/deploy-pve.yml -i inventory.ini -e "@config_vars.yml" --skip-tags base_deployment
-ansible-playbook bare-metal/net/ansible/deploy-network.yml -i inventory.ini -e "@config_vars.yml"
-ansible-playbook templates/ansible/install-basic-templates.yml -i inventory.ini -e "@config_vars.yml"
